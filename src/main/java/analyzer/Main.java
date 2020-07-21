6f513f942761506d43295facfc1e98349ad1c6e1
Igor <GI_Something@protonmail.com>
2020-07-21 17:48:56 +0300

    feat: Measure/show search time.

--- src/main/java/analyzer/Main.java
+++ src/main/java/analyzer/Main.java
@@ -10,11 +10,18 @@ public class Main {
             System.out.println("Error, incorrect number of arguments. Expected: 3, found: " + args.length);
             return;
         }
+        byte[] data;
         try {
-            boolean result = AnalyzerUtils.contains(Files.readAllBytes(Path.of(args[0])), args[1].getBytes());
-            System.out.println(result ? args[2] : "Unknown file type");
+            data = Files.readAllBytes(Path.of(args[0]));
         } catch (IOException e) {
             System.out.println("Something went wrong when trying to read file");
+            return;
         }
+        long start = System.currentTimeMillis();
+        boolean result = AnalyzerUtils.contains(data, args[1].getBytes());
+        long end = System.currentTimeMillis();
+
+        System.out.println(result ? args[2] : "Unknown file type");
+        System.out.println("It took " + ((end - start) / 1000.0) + " seconds");
     }
 }
