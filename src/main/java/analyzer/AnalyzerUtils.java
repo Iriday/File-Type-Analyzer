7ff5a40c26c589218012c4084587be3b74a9a53e
Igor <GI_Something@protonmail.com>
2020-07-24 22:51:34 +0300

    feat: Impl Knuth Morris Pratt alg/ add possibility to specify search alg
    Command line args: 1. Search alg, 2. Filepath, 3. Pattern, 4. Expected type.
    
    Search algorithms: --naive, --KMP.

--- src/main/java/analyzer/AnalyzerUtils.java
+++ src/main/java/analyzer/AnalyzerUtils.java
@@ -1,6 +1,22 @@
 package analyzer;
 
+import java.util.function.BiFunction;
+
 public class AnalyzerUtils {
+    public static BiFunction<byte[], byte[], Boolean> getSearchAlgByName(String name) {
+        if(name.equals("--naive")) return AnalyzerUtils::contains;
+        if(name.equals("--KMP")) return AnalyzerUtils::KMPContains;
+        return null;
+    }
+
+    public static long[] searchAndMeasureTime(BiFunction<byte[], byte[], Boolean> searchAlg, byte[] data, byte[] pattern) {
+        long start = System.currentTimeMillis();
+        boolean found = searchAlg.apply(data, pattern);
+        long end = System.currentTimeMillis();
+
+        return new long[]{end - start, found ? 1 : 0};
+    }
+
     public static boolean contains(byte[] data, byte[] pattern) {
         for (int i = 0; i <= data.length - pattern.length; i++) {
             for (int j = 0; j < pattern.length; j++) {
@@ -13,5 +29,40 @@ public class AnalyzerUtils {
             }
         }
         return false;
+    }
+
+    // Knuth Morris Pratt alg
+    public static boolean KMPContains(byte[] str, byte[] pattern) {
+        int[] prefFun = prefixFunction(pattern);
+        int j = 0;
+        for (int i = 0; i < str.length; i++) {
+            while (j > 0 && str[i] != pattern[j]) {
+                j = prefFun[j - 1];
+            }
+            if (str[i] == pattern[j]) {
+                j++;
+            }
+            if (j == pattern.length) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] prefixFunction(byte[] s) {
+        int[] prefFun = new int[s.length];
+
+        for (int i = 1; i < s.length; i++) {
+            int j = prefFun[i - 1];
+
+            while (j > 0 && s[i] != s[j]) {
+                j = prefFun[j - 1];
+            }
+            if (s[i] == s[j]) {
+                j++;
+            }
+            prefFun[i] = j;
+        }
+        return prefFun;
     }
 }
--- src/main/java/analyzer/Main.java
+++ src/main/java/analyzer/Main.java
@@ -3,25 +3,36 @@ package analyzer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.BiFunction;
+
+import static analyzer.AnalyzerUtils.searchAndMeasureTime;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Error, incorrect number of arguments. Expected: 3, found: " + args.length);
+        if (args.length != 4) {
+            System.out.println("Error, incorrect number of arguments. Expected: 4, found: " + args.length);
             return;
         }
+
         byte[] data;
         try {
-            data = Files.readAllBytes(Path.of(args[0]));
+            data = Files.readAllBytes(Path.of(args[1]));
         } catch (IOException e) {
             System.out.println("Something went wrong when trying to read file");
             return;
         }
-        long start = System.currentTimeMillis();
-        boolean result = AnalyzerUtils.contains(data, args[1].getBytes());
-        long end = System.currentTimeMillis();
 
-        System.out.println(result ? args[2] : "Unknown file type");
-        System.out.println("It took " + ((end - start) / 1000.0) + " seconds");
+        BiFunction<byte[], byte[], Boolean> searchAlg = AnalyzerUtils.getSearchAlgByName(args[0]);
+        if (searchAlg == null) {
+            System.out.println("Unknown search alg. Use: --naive or --KMP");
+            return;
+        }
+
+        outputResult(searchAndMeasureTime(searchAlg, data, args[2].getBytes()), args[3]);
+    }
+
+    public static void outputResult(long[] result, String pattern) {
+        System.out.println(result[1] == 1 ? pattern : "Unknown file type");
+        System.out.println("It took " + (result[0] / 1000.0) + " seconds");
     }
 }
